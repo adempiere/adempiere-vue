5dbbb7f3ad5523a081ebf12a29252ebfc4f0c513
elsiosanchez <elsiossanches@gmail.com>
2021-08-23 22:14:31 -0400

    fixe style

--- src/lang/ADempiere/es.js
+++ src/lang/ADempiere/es.js
@@ -524,7 +524,7 @@ export default {
       taxAmount: 'Monto de Impuesto',
       grandTotal: 'Total General',
       grandTotalConverted: 'Gran Total Convertido',
-      quantityAvailable: 'Cantidad Disponible',
+      quantityAvailable: 'Cantidad',
       upc: 'Código de Barras'
     },
     guideSteps: {
