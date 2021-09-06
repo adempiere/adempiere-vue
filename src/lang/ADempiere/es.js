0d8d8db31aebbd39f74887ca2037c03a2f315c40
elsiosanchez <elsiossanches@gmail.com>
2021-09-06 11:12:18 -0400

    Disable the currency field

--- src/lang/ADempiere/es.js
+++ src/lang/ADempiere/es.js
@@ -462,6 +462,7 @@ export default {
         dayRate: 'Tasa del Día',
         noDayRate: 'No se a generado una tasa del día para la moneda',
         refund: 'Reembolso',
+        paymentMethods: 'Métodos de pago',
         emptyRate: 'Sin tasa de conversión a la fecha',
         TenderType: {
           directDeposit: 'Depósito Directo',
