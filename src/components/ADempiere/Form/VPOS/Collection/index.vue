dca66c457995789bded31d18b1bc2db939d0d2c1
elsiosanchez <elsiossanches@gmail.com>
2021-09-08 16:19:47 -0400

    Validate fields before adding payment

--- src/components/ADempiere/Form/VPOS/Collection/index.vue
+++ src/components/ADempiere/Form/VPOS/Collection/index.vue
@@ -419,7 +419,7 @@ export default {
         fieldsList: fieldLogic,
         isValidate: true
       })
-      if (this.defaulValuePaymentMethods.tender_type === 'X' || this.currentFieldPaymentMethods === this.defaulValuePaymentMethods.uuid) {
+      if (this.defaulValuePaymentMethods.tender_type === 'X' && this.currentFieldPaymentMethods === this.defaulValuePaymentMethods.uuid) {
         return false
       }
       if (this.isEmptyValue(fieldsEmpty)) {
