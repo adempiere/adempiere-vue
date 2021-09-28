a2888bdb6570ac1d23f5c086568f32551c905d02
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2021-09-28 06:56:44 -0400

    feat: Add plop template Composition-API component.

--- plopfile.js
+++ plopfile.js
@@ -3,13 +3,21 @@ const componentGenerator = require('./plop-templates/component/prompt')
 const storeGenerator = require('./plop-templates/store/prompt.js')
 const ADempiereView = require('./plop-templates/ADempiere/view/prompt')
 const ADempiereComponent = require('./plop-templates/ADempiere/component/prompt')
+const ADempiereComponentCompositionApi = require('./plop-templates/ADempiere/component/prompCompositionApi')
 const ADempiereStore = require('./plop-templates/ADempiere/store/prompt')
 
 module.exports = function(plop) {
+  plop.setHelper('capitalize', function(str) {
+    // str is the argument passed to the helper when called
+    str = str || ''
+    return str.slice(0, 1).toUpperCase() + str.slice(1)
+  })
+
   plop.setGenerator('view', viewGenerator)
   plop.setGenerator('component', componentGenerator)
   plop.setGenerator('store', storeGenerator)
   plop.setGenerator('ADempiere View', ADempiereView)
   plop.setGenerator('ADempiere Component', ADempiereComponent)
+  plop.setGenerator('ADempiere Component Composition API', ADempiereComponentCompositionApi)
   plop.setGenerator('ADempiere Store Module', ADempiereStore)
 }
