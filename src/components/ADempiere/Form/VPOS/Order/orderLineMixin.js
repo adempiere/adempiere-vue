0928536319a903d444a592231300611dfaa91073
elsiosanchez <elsiossanches@gmail.com>
2021-09-30 15:14:41 -0400

    Change style of the Mobile UI column view

--- src/components/ADempiere/Form/VPOS/Order/orderLineMixin.js
+++ src/components/ADempiere/Form/VPOS/Order/orderLineMixin.js
@@ -30,13 +30,13 @@ export default {
           columnName: 'LineDescription',
           label: this.$t('form.pos.tableProduct.product'),
           isNumeric: false,
-          size: '345px'
+          size: '400px'
         },
         currentPrice: {
           columnName: 'CurrentPrice',
           label: this.$t('form.productInfo.price'),
           isNumeric: true,
-          size: '165px'
+          size: 'auto'
         },
         quantityOrdered: {
           columnName: 'QtyEntered',
@@ -48,13 +48,13 @@ export default {
           columnName: 'Discount',
           label: this.$t('form.pos.order.discount'),
           isNumeric: true,
-          size: '78px'
+          size: '65px'
         },
         discountTotal: {
           columnName: 'DiscountTotal',
           label: this.$t('form.pos.tableProduct.displayDiscuentAmount'),
           isNumeric: true,
-          size: '110px'
+          size: 'auto'
         },
         discounDisplayTaxIndicator: {
           columnName: 'taxIndicator',
@@ -66,7 +66,7 @@ export default {
           columnName: 'DisplayTaxAmount',
           label: this.$t('form.pos.tableProduct.displayTaxAmount'),
           isNumeric: true,
-          size: '155px'
+          size: 'auto'
         },
         grandTotal: {
           columnName: 'GrandTotal',
@@ -132,37 +132,10 @@ export default {
   watch: {
     isShowKeyLayout(value) {
       if (value) {
-        this.orderLineDefinition.lineDescription.size = '140px'
-        this.orderLineDefinition.currentPrice.size = 'auto'
-      } else {
-        this.orderLineDefinition.lineDescription.size = '300px'
-        this.orderLineDefinition.currentPrice.size = '165px'
-      }
-    },
-    isDisplayTaxAmount(value) {
-      if (!value) {
         this.orderLineDefinition.lineDescription.size = 'auto'
-        this.orderLineDefinition.convertedAmount.size = '350px'
-        this.orderLineDefinition.grandTotal.size = '350px'
       } else {
-        this.orderLineDefinition.lineDescription.size = '345px'
+        this.orderLineDefinition.lineDescription.size = '400px'
       }
-    },
-    isDisplayDiscount(value) {
-      if (!value) {
-        this.orderLineDefinition.lineDescription.size = 'auto'
-        this.orderLineDefinition.convertedAmount.size = '350px'
-        this.orderLineDefinition.grandTotal.size = '350px'
-      } else {
-        this.orderLineDefinition.lineDescription.size = '345px'
-      }
-    }
-  },
-  created() {
-    if (!this.isDisplayTaxAmount || !this.isDisplayDiscount) {
-      this.orderLineDefinition.lineDescription.size = 'auto'
-      this.orderLineDefinition.grandTotal.size = '350px'
-      this.orderLineDefinition.convertedAmount.size = '350px'
     }
   },
   methods: {
