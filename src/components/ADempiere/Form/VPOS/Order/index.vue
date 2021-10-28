fecacc4d9ff660a52f58f94c1acc12d88c1d1da3
elsiosanchez <elsiossanches@gmail.com>
2021-10-28 00:43:12 -0400

    minimal changes

--- src/components/ADempiere/Form/VPOS/Order/index.vue
+++ src/components/ADempiere/Form/VPOS/Order/index.vue
@@ -666,14 +666,14 @@ export default {
       return {}
     },
     currentDocumentType() {
-      if (this.isEmptyValue(this.$store.getters.getCurrentDocumentTypePo)) {
-        return {}
-      }
       if (!this.isEmptyValue(this.$store.getters.posAttributes.currentPointOfSales.documentType)) {
         if (!this.isEmptyValue(this.currentOrder.documentType)) {
           return this.currentOrder.documentType
         }
         return this.$store.getters.getCurrentDocumentTypePos
+      }
+      if (this.isEmptyValue(this.$store.getters.getCurrentDocumentTypePos)) {
+        return {}
       }
       return {}
     },
