334c2956b21350d83a5c40f16160cdd87de21f36
elsiosanchez <elsiossanches@gmail.com>
2021-11-05 09:10:54 -0400

    Default date on collection

--- src/components/ADempiere/Form/VPOS/Collection/index.vue
+++ src/components/ADempiere/Form/VPOS/Collection/index.vue
@@ -603,10 +603,18 @@ export default {
       return this.change
     },
     dateConvertions() {
-      return this.$store.getters.getValueOfField({
+      const date = this.$store.getters.getValueOfField({
         containerUuid: this.containerUuid,
         columnName: 'DateTrx'
       })
+      if (this.isEmptyValue(date) && !this.isEmptyValue(this.currentPointOfSales.currentOrder.dateOrdered)) {
+        return this.formatDateToSend(this.currentPointOfSales.currentOrder.dateOrdered)
+      }
+      if (this.isEmptyValue(date) && this.isEmptyValue(this.currentPointOfSales.currentOrder.dateOrdered)) {
+        const emptyDate = new Date()
+        return emptyDate.getFullYear() + '-' + String(emptyDate.getMonth() + 1).padStart(2, '0') + '-' + String(emptyDate.getDate()).padStart(2, '0')
+      }
+      return date
     },
     selectCurrentFieldCurrency() {
       return this.listCurrency.find(currency => currency.iso_code === this.currentFieldCurrency)
