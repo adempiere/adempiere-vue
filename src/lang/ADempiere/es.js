87337eea8879a6080a1e303071de578e281032e6
Yamel Senih <ysenih@erpya.com>
2021-11-23 19:10:33 -0400

    Update es.js

--- src/lang/ADempiere/es.js
+++ src/lang/ADempiere/es.js
@@ -390,7 +390,7 @@ export default {
     pos: {
       title: 'Punto de Venta',
       priceList: 'Lista de Precio',
-      breakFree: 'Liberar',
+      releaseOrder: 'Liberar',
       optionsPoinSales: {
         title: 'Opciones Rápidas del Punto de Ventas',
         emptyAvailablePaymentMethods: 'Este terminal no tiene configurado los metodos de pagos',
