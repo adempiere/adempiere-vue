976ee54913c47e4df1a71100264bbef426c6366e
elsiosanchez <elsiossanches@gmail.com>
2021-12-14 16:59:30 -0400

    Add Sort Table

--- src/components/ADempiere/Form/VPOS/OrderList/fastOrder.vue
+++ src/components/ADempiere/Form/VPOS/OrderList/fastOrder.vue
@@ -472,7 +472,7 @@ export default {
     listOrdersInvoiced(option) {
       this.isloading = true
       const values = {
-        ...option.params,
+        ...this.currentOptions.params,
         posUuid: this.$store.getters.posAttributes.currentPointOfSales.uuid,
         documentNo: this.input,
         pageToken: this.tokenPage,
