a45620cea11f6b3ac17ab1e66a3ea2f5bad251b6
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2022-03-18 10:42:31 -0400

    feat: Add support to isFieldOnly attribute.

--- src/components/ADempiere/Field/index.vue
+++ src/components/ADempiere/Field/index.vue
@@ -307,12 +307,6 @@ export default {
       })
     },
 
-    isFieldOnly() {
-      if (this.inTable || this.field.isFieldOnly) {
-        return undefined
-      }
-      return this.field.name
-    },
     isSelectCreated() {
       return this.isAdvancedQuery &&
         ['IN', 'NOT_IN'].includes(this.field.operator) &&
