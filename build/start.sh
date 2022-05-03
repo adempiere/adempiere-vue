795d0e16cead9f80af748f98fa52c2a2be33b9b8
elsiosanchez <elsiosanches@gmail.com>
2022-05-03 14:57:48 -0400

    minimal changes

--- build/start.sh
+++ build/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # folder with dist app files
-cd /var/www/adempiere-vue/dist/
+cd /usr/share/nginx/html/static/js
 
 # Set API Proxy connection
 find -name 'app.*.js' -exec sed -i "s|https://api.erpya.com|$API_URL|g" {} \;
 
-# Run app
-tail -f /dev/nul
+# Start nginx web server
+nginx && tail -f /dev/null
